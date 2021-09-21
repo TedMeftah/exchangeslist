29cb2f248998cc7d67d446b682a5f412db7dad78
Mohamed Meftah <mohamed-ted@live.com>
2021-09-21 13:24:14 +0100

    feat: style pagination

--- components/Pagination.tsx
+++ components/Pagination.tsx
@@ -17,7 +17,6 @@ export class Pagination extends Component<Props, state> {
 	total: number
 
 	constructor(props: Props) {
-        console.log(props)
 		super(props)
 		this.perPage = props.perPage || 10
 		this.total = props.total
@@ -25,38 +24,76 @@ export class Pagination extends Component<Props, state> {
 		this.state = { current: props.current }
 	}
 
-    generate() {
-        const delta = 2
-        const left = this.state.current - delta
-        const right = this.state.current + delta + 1
+	generate() {
+		const delta = 2
+		const left = this.state.current - delta
+		const right = this.state.current + delta + 1
 
-        let result = []
-        let lastAdded = 1
-        for (let i = 1; i <= this.numPages; i++) {
-            if (i == 1 || i == this.numPages || i >= left && i < right) {
-                if (lastAdded + 1 < i) {
-                    result.push("...");
-                }
-                result.push(i);
-                lastAdded = i
-            }
-        }
-        
-        return result;
-    }
+		let result = []
+		let lastAdded = 1
+		for (let i = 1; i <= this.numPages; i++) {
+			if (i == 1 || i == this.numPages || (i >= left && i < right)) {
+				if (lastAdded + 1 < i) {
+					result.push('...')
+				}
+				result.push(i)
+				lastAdded = i
+			}
+		}
+
+		return result
+	}
 
 	render() {
 		return (
 			<nav>
-				<ul className="flex">
-					{this.generate().map((i, j) => (
-						<li key={j} className="p-4">
-							<Link href={`?page=${i}`}>
-								<a>{i}</a>
-							</Link>
-						</li>
-					))}
+				<ul>
+					{this.generate().map((page, j) => {
+						if (page === '...') {
+							return <span>...</span>
+						}
+
+						return (
+							<li key={j}>
+								<Link href={`?page=${page}`}>
+									<a className={this.state.current === page ? 'active' : ''}>
+										{page.toString().padStart(2, '0')}
+									</a>
+								</Link>
+							</li>
+						)
+					})}
 				</ul>
+				<style jsx>{`
+					ul {
+						@apply cursor-default flex py-4 px-8 justify-center select-none;
+                        
+						span,
+						a {
+							@apply bg-gray-800 py-2 px-3 text-gray-500 leading-6 block;
+						}
+
+						a {
+							@apply transition-colors;
+
+							&:hover {
+								@apply bg-gray-700 text-gray-400;
+							}
+
+							&.active {
+								@apply pointer-events-none;
+								@apply bg-blue-900 ring-inset ring-blue-700 ring-1 text-blue-400;
+							}
+
+							li:last-child & {
+								@apply rounded-r;
+							}
+							li:first-child & {
+								@apply rounded-l;
+							}
+						}
+					}
+				`}</style>
 			</nav>
 		)
 	}
