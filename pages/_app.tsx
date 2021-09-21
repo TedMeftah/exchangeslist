d2a17bdfd6145dc237595a5a89bcbd107786af15
Mohamed Meftah <mohamed-ted@live.com>
2021-09-21 22:35:57 +0100

    chore: remove framer motion

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,9 @@ import '../styles/fonts.css'
 import '../styles/globals.css'
 
 import type { AppProps } from 'next/app'
-import { AnimateSharedLayout } from 'framer-motion'
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
-		<AnimateSharedLayout>
 			<Component {...pageProps} />
-		</AnimateSharedLayout>
 	)
 }
 export default MyApp
