6a057cd338c190e69610803a751590e577c1cb91
Mohamed Meftah <mohamed-ted@live.com>
2021-09-21 22:18:47 +0100

    feat: add back button

--- pages/exchanges/[exchange].tsx
+++ pages/exchanges/[exchange].tsx
@@ -4,7 +4,8 @@ import { Exchanges } from '../../data/coingecko'
 import { removeUndefined, wait } from '../../helpers'
 
 import Head from 'next/head'
-import { ExternalLinkIcon, GlobeAltIcon } from '@heroicons/react/solid'
+import Link from 'next/link'
+import { ExternalLinkIcon, GlobeAltIcon, HomeIcon } from '@heroicons/react/solid'
 import Exchange from '../../components/Exchange'
 import { useRouter } from 'next/router'
 
@@ -27,14 +28,21 @@ function renderInfo(exchange: ExchangeDetails) {
 }
 
 const ExchangePage: NextPage<Props> = ({ exchange }) => {
-    const router = useRouter()
-    if (router.isFallback) return <></>
-    
+	const router = useRouter()
+	if (router.isFallback) return <></>
+
 	return (
 		<>
 			<Head>
 				<title>{exchange.name} | Exchanges List</title>
 			</Head>
+			<div className="mx-auto max-w-4xl">
+				<Link href="/">
+					<a className="back-button">
+						<HomeIcon />
+					</a>
+				</Link>
+			</div>
 			<div className="container">
 				<div className="header">
 					<Exchange.Logo className="logo" src={exchange.image} alt={exchange.name} />
@@ -58,74 +66,84 @@ const ExchangePage: NextPage<Props> = ({ exchange }) => {
 				</div>
 
 				<p className="content">{exchange.description}</p>
+			</div>
 
-				<style jsx>{`
-					.container {
-						@apply rounded bg-gray-700 border-gray-600 border-1 shadow-lg p-4 items-center relative;
-						@apply mx-auto mt-10 w-full max-w-4xl;
+			<style jsx>{`
+				.back-button {
+					@apply transition-colors text-gray-700;
+					& :global(svg) {
+						@apply h-6 w-6;
 					}
-					.container :global(.logo) {
-						@apply rounded-full h-24 w-24 sm:h-30 sm:w-30;
-						@apply mx-auto -mt-10;
+
+					&:hover {
+						@apply text-gray-600;
+					}
+				}
+				.container {
+					@apply rounded bg-gray-700 border-gray-600 border-1 shadow-lg p-4 items-center relative;
+					@apply mx-auto mt-5 w-full max-w-4xl;
+				}
+				.container :global(.logo) {
+					@apply rounded-full h-24 w-24 sm:h-30 sm:w-30;
+					@apply mx-auto -mt-15;
+				}
+				.info {
+					@apply my-4;
+					h1 {
+						@apply font-bold tracking-wider text-2xl text-gray-200;
+						@apply text-center;
+						& span {
+							@apply font-light text-gray-400;
+						}
+					}
+					p {
+						@apply flex text-gray-500 items-center justify-center;
+					}
+				}
+
+				.button {
+					@apply rounded flex font-semibold bg-lime-500 ring-inset text-sm py-2 px-3 ring-lime-400 ring-1 text-green-900 justify-center items-center;
+					@apply transition-colors;
+					@apply mb-3;
+					&:hover {
+						@apply bg-lime-400;
+					}
+				}
+				.content {
+					@apply text-lg text-gray-400;
+					&:not(:empty) {
+						@apply mt-4;
+					}
+				}
+				@screen md {
+					.container {
+						@apply mt-5 p-6;
 					}
 					.header {
-						@apply mb-4;
+						@apply flex;
 					}
 					.info {
-						@apply my-4;
+						@apply mr-auto my-0 ml-5;
 						h1 {
-							@apply font-bold tracking-wider text-2xl text-gray-200;
-							@apply text-center;
-							& span {
-								@apply font-light text-gray-400;
-							}
+							@apply text-left;
 						}
 						p {
-							@apply flex text-gray-500 items-center justify-center;
+							@apply justify-start;
 						}
 					}
 
 					.button {
-						@apply rounded flex font-semibold bg-lime-500 ring-inset text-sm py-2 px-3 ring-lime-400 ring-1 text-green-900 justify-center items-center;
-						@apply transition-colors;
-						@apply mb-3;
-						&:hover {
-							@apply bg-lime-400;
-						}
+						@apply order-2 mb-0 ml-2;
 					}
-					.content {
-						@apply text-lg text-gray-400;
-					}
-					@screen md {
-						.container {
-							@apply mt-20 p-6;
-						}
-						.header {
-							@apply flex;
-						}
-						.info {
-							@apply mr-auto my-0 ml-5;
-							h1 {
-								@apply text-left;
-							}
-							p {
-								@apply justify-start;
-							}
-						}
 
-						.button {
-							@apply order-2 mb-0 ml-2;
-						}
-
-						.navigation {
-							@apply flex items-start;
-						}
-						.container :global(.logo) {
-							@apply mx-0 -mt-15;
-						}
+					.navigation {
+						@apply flex items-start;
 					}
-				`}</style>
-			</div>
+					.container :global(.logo) {
+						@apply mx-0 -mt-15;
+					}
+				}
+			`}</style>
 		</>
 	)
 }
